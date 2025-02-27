36c5a03d309926db9d87708d5329c54c103e7965
mmdcloud <madmaxcloudpractice@gmail.com>
2025-02-27 12:56:37 +0530

    update: restructured tf folders

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,134 @@
+resource "aws_sqs_queue" "event-source-mapping-queue" {
+  name                       = "event-source-mapping-queue"
+  delay_seconds              = 0
+  max_message_size           = 262144
+  visibility_timeout_seconds = 30
+  message_retention_seconds  = 345600
+  receive_wait_time_seconds  = 0
+  tags = {
+    Name = "event-source-mapping-queue"
+  }
+}
+
+# DynamoDB Table For storing media records
+resource "aws_dynamodb_table" "event-source-mapping-records" {
+  name           = "event-source-mapping-records"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "id"
+  range_key      = "name"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+
+  tags = {
+    Name = "event-source-mapping"
+  }
+}
+
+# Lambda Function Role
+resource "aws_iam_role" "event-source-mapping-function-role" {
+  name               = "event-source-mapping-function-role"
+  assume_role_policy = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }
+    EOF
+  tags = {
+    Name = "event-source-mapping"
+  }
+}
+
+# Lambda Function Policy
+resource "aws_iam_policy" "event-source-mapping-function-policy" {
+  name        = "event-source-mapping-function-policy"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+      {
+          "Action": [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource": "arn:aws:logs:*:*:*",
+          "Effect": "Allow"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+            "sqs:ReceiveMessage",
+            "sqs:GetQueueAttributes",
+            "sqs:DeleteMessage"
+        ],
+        "Resource": "${aws_sqs_queue.event-source-mapping-queue.arn}"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+            "dynamodb:PutItem",
+            "dynamodb:DescribeTable"
+        ],
+        "Resource": "${aws_dynamodb_table.event-source-mapping-records.arn}"
+       }          
+      ]
+    }
+    EOF
+  tags = {
+    Name = "event-source-mapping"
+  }
+}
+
+# Lambda Function Role-Policy Attachment
+resource "aws_iam_role_policy_attachment" "event-source-mapping-function-policy-attachment" {
+  role       = aws_iam_role.event-source-mapping-function-role.name
+  policy_arn = aws_iam_policy.event-source-mapping-function-policy.arn
+}
+
+resource "aws_lambda_function" "event-source-mapping-function" {
+  filename      = "./files/lambda.zip"
+  function_name = "event-source-mapping-function"
+  role          = aws_iam_role.event-source-mapping-function-role.arn
+  handler       = "lambda.lambda_handler"
+  runtime       = "python3.12"
+  depends_on    = [aws_iam_role_policy_attachment.event-source-mapping-function-policy-attachment]
+  tags = {
+    Name = "event-source-mapping"
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_event_trigger" {
+  event_source_arn = aws_sqs_queue.event-source-mapping-queue.arn
+  function_name    = aws_lambda_function.event-source-mapping-function.arn
+  enabled          = true
+  batch_size       = 10
+}
+
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "api_gateway_execution_role" {
