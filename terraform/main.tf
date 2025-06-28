54a4debef24e3dbf645545017e8d14499d413e07
mmdcloud <madmaxcloudpractice@gmail.com>
2025-06-28 06:29:31 +0530

    update: added various terraform modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,135 +1,142 @@
-resource "aws_sqs_queue" "event-source-mapping-queue" {
-  name                       = "event-source-mapping-queue"
-  delay_seconds              = 0
-  max_message_size           = 262144
-  visibility_timeout_seconds = 30
-  message_retention_seconds  = 345600
-  receive_wait_time_seconds  = 0
-  tags = {
-    Name = "event-source-mapping-queue"
-  }
+data "aws_caller_identity" "current" {}
+
+# Lambda Function Code Bucket
+module "lambda_function_code_bucket" {
+  source      = "./modules/s3"
+  bucket_name = "event-source-mapping-function-code-bucket-${data.aws_caller_identity.current.account_id}"
+  objects = [
+    {
+      key    = "lambda.zip"
+      source = "./files/lambda.zip"
+    }
+  ]
+  versioning_enabled = "Enabled"
+  cors = [
+    {
+      allowed_headers = ["*"]
+      allowed_methods = ["PUT", "POST", "GET"]
+      allowed_origins = ["*"]
+      max_age_seconds = 3000
+    }
+  ]
+  force_destroy = true
 }
 
-# DynamoDB Table For storing media records
-resource "aws_dynamodb_table" "event-source-mapping-records" {
-  name           = "event-source-mapping-records"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "id"
-  range_key      = "name"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
-
-  attribute {
-    name = "name"
-    type = "S"
-  }
-
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = true
-  }
-
-  tags = {
-    Name = "event-source-mapping"
-  }
+# DynamoDB Table
+module "dynamodb" {
+  source = "./modules/dynamodb"
+  name   = "event-source-mapping-records"
+  attributes = [
+    {
+      name = "id"
+      type = "S"
+    },
+    {
+      name = "name"
+      type = "S"
+    }
+  ]
+  billing_mode          = "PROVISIONED"
+  hash_key              = "id"
+  range_key             = "name"
+  read_capacity         = 20
+  write_capacity        = 20
+  ttl_attribute_name    = "TimeToExist"
+  ttl_attribute_enabled = true
 }
 
-# Lambda Function Role
-resource "aws_iam_role" "event-source-mapping-function-role" {
-  name               = "event-source-mapping-function-role"
+# SQS
+module "sqs" {
+  source                        = "./modules/sqs"
+  queue_name                    = "event-source-mapping-queue"
+  delay_seconds                 = 0
+  maxReceiveCount               = 3
+  dlq_message_retention_seconds = 86400
+  dlq_name                      = "event-source-mapping-dlq"
+  max_message_size              = 262144
+  message_retention_seconds     = 345600
+  visibility_timeout_seconds    = 30
+  receive_wait_time_seconds     = 0
+  policy                        = ""
+}
+
+# Lambda function IAM Role
+module "lambda_function_iam_role" {
+  source             = "./modules/iam"
+  role_name          = "_event_source_mapping_lambda_function_iam_role"
+  role_description   = "_event_source_mapping_lambda_function_iam_role"
+  policy_name        = "_event_source_mapping_lambda_function_iam_policy"
+  policy_description = "_event_source_mapping_lambda_function_iam_policy"
   assume_role_policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                  "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+    }
+    EOF
+  policy             = <<EOF
     {
       "Version": "2012-10-17",
       "Statement": [
         {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "lambda.amazonaws.com"
-          },
+            "Action": [
+              "logs:CreateLogGroup",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents",
+              "mediaconvert:*"
+            ],
+            "Resource": "arn:aws:logs:*:*:*",
+            "Effect": "Allow"
+        },
+        {
           "Effect": "Allow",
-          "Sid": ""
+          "Action": [
+            "sqs:ReceiveMessage",
+            "sqs:GetQueueAttributes",
+            "sqs:DeleteMessage"
+          ],
+          "Resource": "${module.sqs.arn}"
+        },
+        {
+          "Effect": "Allow",
+          "Action": [
+            "dynamodb:PutItem",
+            "dynamodb:DescribeTable"
+          ],
+          "Resource": "${module.dynamodb.arn}"
         }
       ]
     }
     EOF
-  tags = {
-    Name = "event-source-mapping"
-  }
 }
 
-# Lambda Function Policy
-resource "aws_iam_policy" "event-source-mapping-function-policy" {
-  name        = "event-source-mapping-function-policy"
-  description = "AWS IAM Policy for managing aws lambda role"
-  policy      = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-      {
-          "Action": [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents"
-          ],
-          "Resource": "arn:aws:logs:*:*:*",
-          "Effect": "Allow"
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-            "sqs:ReceiveMessage",
-            "sqs:GetQueueAttributes",
-            "sqs:DeleteMessage"
-        ],
-        "Resource": "${aws_sqs_queue.event-source-mapping-queue.arn}"
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-            "dynamodb:PutItem",
-            "dynamodb:DescribeTable"
-        ],
-        "Resource": "${aws_dynamodb_table.event-source-mapping-records.arn}"
-       }          
-      ]
-    }
-    EOF
-  tags = {
-    Name = "event-source-mapping"
-  }
-}
-
-# Lambda Function Role-Policy Attachment
-resource "aws_iam_role_policy_attachment" "event-source-mapping-function-policy-attachment" {
-  role       = aws_iam_role.event-source-mapping-function-role.name
-  policy_arn = aws_iam_policy.event-source-mapping-function-policy.arn
-}
-
-resource "aws_lambda_function" "event-source-mapping-function" {
-  filename      = "./files/lambda.zip"
+# Lambda function to process media files
+module "lambda_function" {
+  source        = "./modules/lambda"
   function_name = "event-source-mapping-function"
-  role          = aws_iam_role.event-source-mapping-function-role.arn
+  role_arn      = module.lambda_function_iam_role.arn
+  env_variables = {}
   handler       = "lambda.lambda_handler"
   runtime       = "python3.12"
-  depends_on    = [aws_iam_role_policy_attachment.event-source-mapping-function-policy-attachment]
-  tags = {
-    Name = "event-source-mapping"
-  }
+  s3_bucket     = module.lambda_function_code_bucket.bucket
+  s3_key        = "lambda.zip"
+  depends_on    = [module.lambda_function_iam_role, module.lambda_function_code_bucket]
 }
 
 resource "aws_lambda_event_source_mapping" "sqs_event_trigger" {
-  event_source_arn = aws_sqs_queue.event-source-mapping-queue.arn
-  function_name    = aws_lambda_function.event-source-mapping-function.arn
+  event_source_arn = module.sqs.arn
+  function_name    = module.lambda_function.arn
   enabled          = true
   batch_size       = 10
 }
-
-data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "api_gateway_execution_role" {
   name = "api_gateway_execution_role"
@@ -181,7 +188,7 @@ resource "aws_api_gateway_integration" "event-source-mapping-api-integration" {
   integration_http_method = "POST"
   type                    = "AWS"
   credentials             = aws_iam_role.api_gateway_execution_role.arn
-  uri                     = "arn:aws:apigateway:${var.region}:sqs:path/${data.aws_caller_identity.current.account_id}/${aws_sqs_queue.event-source-mapping-queue.name}"
+  uri                     = "arn:aws:apigateway:${var.region}:sqs:path/${data.aws_caller_identity.current.account_id}/${module.sqs.name}"
   request_parameters = {
     "integration.request.header.Content-Type" = "'application/x-www-form-urlencoded'"
   }
